405599202e37b735d38c751f1f301ac1d6714039
Erik Westrup <erik.westrup@gmail.com>
2013-09-14 19:20:30 +0200

    Git ignore built files.

---
+++ src/spotify-playlist-exporter.c
@@ -0,0 +1,12 @@
+#include "spotify-playlist-exporter.h"
+
+#include <stdlib.h>
+
+#include <libspotify/api.h>
+
+/*int main(int argc, const char *argv[])*/
+int main()
+{
+	
+	return EXIT_SUCCESS;
+}
