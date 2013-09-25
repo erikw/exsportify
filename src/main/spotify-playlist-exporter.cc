5ac268e7d5c1943ea9273d8bfbb761de96f98b6d
Erik Westrup <erik.westrup@gmail.com>
2013-09-25 22:14:22 +0200

    A start on the rewrite. New make file.

---
+++ src/main/spotify-playlist-exporter.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+
+enum states{logged_in, plc_loaded, all_pl_printed, logged_out};
+
+
+int main(int argc, const char *argv[])
+{
+	cout << "running" << endl;
+	return 0;
+}
